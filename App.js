403edff08a589620053631d498f9d1b783430512
PlabanJr <plabanjr@gmail.com>
2020-05-20 15:22:53 +0530

    feat(): feat peripheral capability

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  View,
+  SafeAreaView,
+} from 'react-native';
 import {BleManager} from 'react-native-ble-plx';
+import Peripheral, {Service, Characteristic} from 'react-native-peripheral';
 import database from '@react-native-firebase/database';
+import find from 'lodash/find';
 
 const App = () => {
-  // const [devices, setdevices] = useState({});
+  // const [devices, setdevices] = useState([]);
   const manager = React.useRef();
+  const serviceUuid = 'ebed0e09-033a-4bfe-8dcc-20a04fad944e';
   manager.current = new BleManager({
     restoreStateIdentifier: 'restoreStateIdentifier',
     restoreStateFunction: () => {},
@@ -13,6 +23,35 @@ const App = () => {
   });
 
   useEffect(() => {
+    Peripheral.onStateChanged((state) => {
+      if (state === 'poweredOn') {
+        Peripheral.addService(
+          new Service({
+            // these are just randomly generated UUIDs
+            uuid: serviceUuid,
+            characteristics: [
+              new Characteristic({
+                uuid: 'c36e1c5a-fc6e-48c8-9a8e-d0b350399d0e',
+                value: '...',
+              }),
+              new Characteristic({
+                uuid: 'fbc47809-76ce-44fa-a2f0-676b95615472',
+                onReadRequest: async () => this.state.value,
+                onWriteRequest: async (value) => this.setState({value}),
+              }),
+            ],
+          }),
+        ).then(() => {
+          Peripheral.startAdvertising({
+            name: 'POC BLE Test GA',
+            serviceUuids: [serviceUuid],
+          })
+            .then(() => console.log('Advertising Success'))
+            .catch((e) => console.log('Advertising Error', e));
+        });
+      }
+    });
+
     const subscription = manager.current.onStateChange((state) => {
       console.log('>> subscription', state);
       if (state === 'PoweredOn') {
@@ -24,55 +63,74 @@ const App = () => {
 
   const scanAndConnect = () => {
     console.log('>> SCAN');
-    manager.current.startDeviceScan(null, null, (error, device) => {
-      console.log('>>> device', device);
-      if (error) {
-        console.log(error, '>> err');
-        return;
-      }
+    manager.current.startDeviceScan(
+      ['00001809-0000-1000-8000-00805f9b34fb'],
+      null,
+      (error, device) => {
+        console.log('>>> device', device);
+        if (error) {
+          console.log(error, '>> err');
+          return;
+        }
 
-      if (device) {
-        database()
-          .ref('/sccannedDevices')
-          .update({
-            [device.id]: device.id,
-          })
-          .then(() => console.log('Data set.'))
-          .catch((e) => console.log('Error DB', e));
-      }
+        if (device) {
+          database()
+            .ref('/sccannedDevices')
+            .update({
+              [device.id]: device.id,
+            })
+            .then(() => console.log('Data set.'))
+            .catch((e) => console.log('Error DB', e));
+        }
 
-      // Check if it is a device you are looking for based on advertisement data
-      // or other criteria.
-      // Stop scanning as it's not necessary if you are scanning for one device.
-      // manager.current.stopDeviceScan();
-      // Proceed with connection.
-
-      // device
-      //   .connect()
-      //   .then((connectedDevice) => {
-      //     return connectedDevice.discoverAllServicesAndCharacteristics();
-      //   })
-      //   .then((newDevice) => {
-      //     // Do work on device with services and characteristics
-      //     console.log('>> characteristics', newDevice);
-      //   })
-      //   .catch((e) => {
-      //     console.log('>> err', e);
-      //   });
-    });
-  };
-
-  const search = () => {
-    scanAndConnect();
+        // device
+        //   .connect()
+        //   .then((connectedDevice) => {
+        //     return connectedDevice.discoverAllServicesAndCharacteristics();
+        //   })
+        //   .then((newDevice) => {
+        //     // Do work on device with services and characteristics
+        //     console.log('>> characteristics', newDevice);
+        //   })
+        //   .catch((e) => {
+        //     console.log('>> err', e);
+        //   });
+      },
+    );
   };
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Text style={styles.coronaText}>GoCoronaGO</Text>
 
-      <TouchableOpacity onPress={search} style={styles.search}>
-        <Text>Search</Text>
+      <TouchableOpacity onPress={scanAndConnect} style={styles.search}>
+        <Text style={styles.buttonText}>Search</Text>
       </TouchableOpacity>
+
+      {/* <View style={styles.listWrapper}>
+        <FlatList
+          data={devices}
+          renderItem={({item, id}) => {
+            console.log('>>> item', item.isConnectable);
+
+            return (
+              <View style={styles.listItem}>
+                <Text style={styles.listItemHead}>ID:</Text>
+                <Text>{item.id}</Text>
+
+                <Text style={styles.listItemHead}>isConnectable:</Text>
+                <Text>{item.isConnectable ? 'true' : 'false'} </Text>
+
+                <Text style={styles.listItemHead}>NAME:</Text>
+                <Text>{item.name} </Text>
+
+                <Text style={styles.listItemHead}>SERVICEUUID: </Text>
+                <Text>{item.serviceUUIDs} </Text>
+              </View>
+            );
+          }}
+        />
+      </View> */}
     </SafeAreaView>
   );
 };
@@ -87,6 +145,10 @@ const styles = StyleSheet.create({
   coronaText: {
     fontSize: 40,
   },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
   search: {
     width: '80%',
     padding: 10,
@@ -94,6 +156,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#0779e4',
     borderRadius: 20,
+  },
+  listWrapper: {
+    backgroundColor: '#fff',
+    flex: 1,
+    width: '90%',
+    borderRadius: 15,
+  },
+  listItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    padding: 10,
+  },
+  listItemHead: {
+    color: '#0779e4',
+    fontWeight: 'bold',
+    marginTop: 5,
   },
 });
 
